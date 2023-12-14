ce5f69e4ab751c3870874328df70878445f1e5a2
ron029 <bonifacioronmark@gmail.com>
2023-12-14 16:06:39 +0800

    fix axios search

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -32,7 +32,7 @@ const dataSearch = {
 }
 const baseURL = 'https://api01.hmi.com.ph';
 const configToken = {
-  baseURL: baseURL,
+  baseURL,
   headers: {
     'Content-Type': 'application/x-www-form-urlencoded'
   }
