4e54b6261d4a1c147e9baf99375bb032fade99b2
ron029 <bonifacioronmark@gmail.com>
2023-12-14 15:07:41 +0800

    fix axios search

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -70,7 +70,6 @@ export default {
       dataSearch.dob = this.inputTextBirthDate;
 
       const configSearch = {
-        baseURL: "https://api01.hmi.com.ph",
         headers: {
           "Content-Type": "application/json",
           "Authorization": 'Bearer ' + this.data.access_token
@@ -80,7 +79,7 @@ export default {
       try {
         const axiosInstance = axios.create(configSearch); // Create an instance with custom headers
 
-        const response = await axiosInstance.post('/api03/da/member', dataSearch);
+        const response = await axiosInstance.post('https://api01.hmi.com.ph/api03/da/member', dataSearch, configSearch);
         this.data_result = response.data;
 
         console.log('data length: ', this.data_result.length);
