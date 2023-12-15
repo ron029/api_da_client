5f61f3fbd6363d7537d4d4d1fa1403e64db2b1ef
ron029 <bonifacioronmark@gmail.com>
2023-12-15 14:33:08 +0800

    fix-cors

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,100 +2,113 @@
   <div class="hello">
     <h1>Get Token</h1>
     <button @click="getToken">Get token</button>
-    <div v-if="token">
-      <h2>Access Token:</h2>
-      <pre>{{ token }}</pre>
+    <div v-if="data">
+      <h2>Data:</h2>
+      <pre>{{ data }}</pre>
     </div>
     <h1>Fetch Data</h1>
     <input type="text" v-model="inputTextname" placeholder="Enter name" />
-    <input type="text" v-model="inputTextBirthDate" placeholder="Enter date of birth" />
+    <input type="text" v-model="inputTextBirthDate" placeholder="Enter date of birthdate" />
     <p v-if="name">Name: {{ name }}</p>
     <p v-if="birthDate">Birthdate: {{ birthDate }}</p>
     <button @click="fetchData">Fetch Data</button>
-    <p v-if="result === 1">No data found.</p>
-    <div v-else-if="data_result">
-      <h2>Data Result:</h2>
-      <pre>{{ data_result }}</pre>
-    </div>
+    <p v-if="result">Search Result: {{ data_result }}</p>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
 
-const instance = axios.create({
-  baseURL: 'https://api01.hmi.com.ph', // Use the base URL defined in vue.config.js
-  timeout: 1000,
-});
+const dataToken = new URLSearchParams();
+dataToken.append('client_id', '1234-ABDE');
+dataToken.append('scope', 'onlc:read');
+dataToken.append('grant_type', 'clients_credentials');
+dataToken.append('client_secret', 'super_secret_key');
+
+const dataSearch = {
+  name: "",
+  dob: "",
+  nric: ""
+}
+const baseURL = 'https://api01.hmi.com.ph';
+const configToken = {
+  baseURL: baseURL,
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded'
+  }
+};
+
 export default {
   name: 'HelloWorld',
   data() {
     return {
-      token: '',
+      data: '',
       name: '',
       birthDate: '',
       inputTextname: 'John Doe',
       inputTextBirthDate: '1990-05-15',
-      data_result: null,
+      data_result: '',
       result: 0,
-    };
+    }
   },
   methods: {
     async getToken() {
       try {
-        const response = await instance.post('/api03/da/generateToken');
-        this.token = response.data.access_token;
+        const response = await axios.post('/api03/da/generateToken', dataToken, configToken);
+        this.data = response.data;
+        console.log('Data:', this.data);
+        return this.data; // Returning the data in case you need to use it elsewhere
       } catch (error) {
-        console.error('Error fetching token:', error);
-        // Handle error if needed
+        console.error('Error fetching data:', error);
+        throw error; // Throwing the error for further handling if needed
       }
     },
     async fetchData() {
-      try {
-        const dataSearch = {
-          name: this.inputTextname,
-          dob: this.inputTextBirthDate,
-        };
+      this.name = this.inputTextname;
+      this.birthDate = this.inputTextBirthDate;
 
-        /*
-        const headers = {
-          'Content-Type': 'application/json',
-          Authorization: 'Bearer ' + this.token,
-        };
+      dataSearch.name = this.inputTextname,
+      dataSearch.dob = this.inputTextBirthDate
 
-        const requestOptions = {
-          method: 'POST',
-          headers: headers,
-          body: JSON.stringify(dataSearch),
-        };
-        */
-       
-        const response = await instance.post('/member', dataSearch);
-        if (!response.ok) {
-          if (response.status === 404) {
-            throw new Error('No data found');
+      const headers = {
+        "Content-Type": "application/json",
+        'Authorization': 'Bearer ' + this.data.access_token
+      };
+
+      const requestOptions = {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify(dataSearch)
+      };
+
+      const url = 'https://api01.hmi.com.ph/api03/da/member';
+      fetch(url, requestOptions)
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok.');
           }
-          throw new Error('Network response was not ok.');
-        }
+          return response.json();
+        })
+        .then(data => {
+          this.data_result = data;
 
-        const data = await response.json();
-        this.data_result = data;
-
-        if (this.data_result.length === 0) {
-          this.result = 1;
-        } else {
-          this.result = 0;
-        }
-      } catch (error) {
-        console.error('Error fetching data:', error);
-        if (error.message === 'No data found') {
-          alert('No data found. Please try a different search.');
-        }
-        // Handle other errors if needed
-      }
-    },
-  },
-};
+          console.log('data length:', this.data_result.length);
+          if (this.data_result.length === 0) {
+            this.result = 1;
+          }
+          console.log('Data:', this.data_result);
+        })
+        .catch(error => {
+          console.error('Error fetching data:', error);
+          if (error.message === 'No data found') {
+            // Your code to handle the 404 error and show a message to the client
+            // For example:
+            alert('No data found. Please try a different search.');
+          }
+        });
+    }
+  }
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
