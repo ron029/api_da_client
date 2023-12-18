cbcfae25c158434422a79518b3c37a1caf5cfbdf
ron029 <bonifacioronmark@gmail.com>
2023-12-18 15:48:33 +0800

    fix-cors

--- vue.config.js
+++ vue.config.js
@@ -4,20 +4,34 @@
 // });
 
 
+// module.exports = {
+//   transpileDependencies: true,
+//   devServer: {
+//     proxy: {
+//       '/api03/da': {
+//         target: 'https://api01.hmi.com.ph/',
+//         ws: true,
+//         changeOrigin: true,
+//         https: true,
+//         hotOnly: false,
+//         disableHostCheck: true,
+//         headers: {
+//           'Access-Control-Allow-Origin': '*'
+//         }
+//       },
+//     },
+//   },
+// };
+
 module.exports = {
-  transpileDependencies: true,
   devServer: {
     proxy: {
-      '/api03/da': {
-        target: 'https://api01.hmi.com.ph/',
-        ws: true,
+      '/api': {
+        target: 'https://api01.hmi.com.ph',
         changeOrigin: true,
-        https: true,
-        hotOnly: false,
-        disableHostCheck: true,
-        headers: {
-          'Access-Control-Allow-Origin': '*'
-        }
+        pathRewrite: {
+          '^/api': '', // Remove /api from the request path
+        },
       },
     },
   },
