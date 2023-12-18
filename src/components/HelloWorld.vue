ea602ce1ad1d8e2af077d810ed9479a2c2df2808
ron029 <bonifacioronmark@gmail.com>
2023-12-19 07:41:31 +0800

    fix-cors

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -92,10 +92,19 @@ export default {
         })
         .catch(error => {
           
-          if (error.message === "AxiosError") {
-            console.error('Error fetching data:', error);
-            console.log('internal error')
+          // Handle the error here
+          if (error.response) {
+            // The request was made and the server responded with a status code
+            console.error('Response status:', error.response.status);
+            console.error('Response data:', error.response.data);
+          } else if (error.request) {
+            // The request was made but no response was received
+            console.error('Request made but no response received:', error.request);
+          } else {
+            // Something happened in setting up the request that triggered an error
+            console.error('Error:', error.message);
           }
+          console.error('Error config:', error.config);
         });
     }
   }
