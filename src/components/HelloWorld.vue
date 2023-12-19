1415be77aabc13aa2704d5970d23a76548e5c887
ron029 <bonifacioronmark@gmail.com>
2023-12-19 12:45:24 +0800

    fix-cors

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -73,7 +73,7 @@ export default {
         headers:  {
           'Accept': 'application/json',
           'Content-Type': 'application/json;charset=UTF-8',
-          'Authorization': 'Bearer ' + this.data.access_token
+          'Authorization': 'Bearer ' + this.data.access_token + 'asdf'
         },
         validateStatus: function (status) {
           return status >= 200 && status < 400
